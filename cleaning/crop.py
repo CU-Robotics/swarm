b9b2d333f90e5cbc3e12e76e0b70a30e5474655b
Daniel Zhu <daniel.zhu@colorado.edu>
2025-10-08 22:42:19 -0600

    added a plate number indicator on images so we don't overwrite images where there are multiple plates

--- cleaning/crop.py
+++ cleaning/crop.py
@@ -16,20 +16,30 @@ def main():
     for data, src, dst in ctx.rows():
         image = cv2.imread(str(src))
 
-        for plate in data["labels"].get("plates", []):
+        for i, plate in enumerate(data["labels"].get("plates", [])):
             # get bounding box
             x, y, w, h = plate
 
+            # find out largest side, if the width is greater, we want to make the height match
+            # If the height is greater, we want to keep as usual
+            l_side = max(w, h)
+
             # crop image to bounding box
-            cropped = image[(y + h//2 - w//2):(y + h //2 + w //2), x:(x + w)]
+            cropped = image[(y + h//2 - l_side//2):(y + h //2 + l_side//2), x:(x + w)]
 
             # resize image to target size
             cropped = cv2.resize(cropped, (target_x_pixels, target_y_pixels))
 
             bw = cv2.cvtColor(cropped, cv2.COLOR_BGR2GRAY)
             
-            # save cropped image
-            cv2.imwrite(str(dst), bw)
+            # If there are multiple plates, give them unique names so we don't overwrite the cropped images
+            filename = str(dst.stem)
+
+            if len(data["labels"].get("plates", [])) > 1:
+                filename = filename + f"-{i}"
+            
+            filename = dst.parent / (filename + dst.suffix)
+            cv2.imwrite(str(filename), bw)
         
         
     # update metadata
