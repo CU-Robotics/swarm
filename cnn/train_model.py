1e91d0f8b65c703f60919f9d1e72663b10c0b819
Daniel Zhu <daniel.zhu@colorado.edu>
2025-10-14 18:35:40 -0600

    added tqdm to training

--- cnn/train_model.py
+++ cnn/train_model.py
@@ -7,15 +7,18 @@ from torch.utils.data import random_split, DataLoader
 import torch.nn as nn
 import torch.optim as optim
 import ConvNet
+from tqdm import tqdm
 
 
 device = torch.accelerator.current_accelerator().type if torch.accelerator.is_available() else "cpu"
 print(f'Using device: {device}')
 
 def train(dataloader: DataLoader, model:nn.Module, loss_fn, optimizer):
-    size = len(dataloader.dataset)
     model.train()
-    for batch, (X, y) in enumerate(dataloader):
+    running_loss = 0.0
+
+    loop = tqdm(enumerate(dataloader), total=len(dataloader), desc="Training", leave=True)
+    for batch, (X, y) in loop:
         X, y = X.to(device), y.to(device)
 
         # Compute prediction error
@@ -27,9 +30,11 @@ def train(dataloader: DataLoader, model:nn.Module, loss_fn, optimizer):
         optimizer.step()
         optimizer.zero_grad()
 
-        if batch % 100 == 0:
-            loss, current = loss.item(), (batch + 1) * len(X)
-            # print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+        running_loss += loss.item()
+        loop.set_postfix(loss=loss.item())
+
+    avg_loss = running_loss / len(dataloader)
+    return avg_loss
 
 def test(dataloader: DataLoader, model:nn.Module, loss_fn):
     size = len(dataloader.dataset)
@@ -37,7 +42,8 @@ def test(dataloader: DataLoader, model:nn.Module, loss_fn):
     model.eval()
     test_loss, correct = 0, 0
     with torch.no_grad():
-        for X, y in dataloader:
+        loop = tqdm(dataloader, total=len(dataloader), desc="Validation", leave=False)
+        for X, y in loop:
             X, y = X.to(device), y.to(device)
             pred = model(X)
             test_loss += loss_fn(pred, y).item()
@@ -45,7 +51,7 @@ def test(dataloader: DataLoader, model:nn.Module, loss_fn):
     
     test_loss /= num_batches
     correct /= size
-    # print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+
     return (test_loss, correct)
 
 def multi_train(train_dataloader: DataLoader, val_dataloader: DataLoader, model:nn.Module, loss_fn, optimizer, epochs: int):
@@ -60,11 +66,12 @@ def multi_train(train_dataloader: DataLoader, val_dataloader: DataLoader, model:
         train_loss, train_acc = test(train_dataloader, model, loss_fn)
         train_losses.append(train_loss)
         train_accuracies.append(train_acc)
-        
+        print(f"\nTraining Loss: {round(train_loss, 5)}\nTraining Accuracy: {round(train_acc, 5)}\n")
+
         val_loss, val_acc = test(val_dataloader, model, loss_fn)
         val_losses.append(val_loss)
         val_accuracies.append(val_acc)
-        print(f"Validation - Loss: {round(val_loss, 5)}, Accuracy: {round(val_acc, 5)}\n")
+        print(f"Validation Loss: {round(val_loss, 5)}\nValidation Accuracy: {round(val_acc, 5)}\n")
     
 
     return train_losses, val_losses, train_accuracies, val_accuracies
@@ -85,7 +92,7 @@ def main():
     # print("Parent dir: " + parent_dir + "\n")
     # print("Reading from directory: " + datadir + "\n")
 
-    batch_size = 32
+    batch_size = 16
 
     transform = transforms.Compose([
         transforms.ToTensor(),
