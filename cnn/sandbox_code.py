d6b420746313cc23f8ac25a05b4bbd668557f1ae
Daniel Zhu <daniel.zhu@colorado.edu>
2025-10-28 20:14:46 -0600

    please work

--- cnn/sandbox_code.py
+++ cnn/sandbox_code.py
@@ -1,23 +1,29 @@
 import torch
 import os
 import torchvision.transforms as transforms
-
+from PIL import Image
 
 # load model
-
 model = torch.load(os.getcwd() + '/models/full_model.pth', map_location=torch.device('cpu'), weights_only=False)
 
-# 100, 100 fake image
-fake_image = torch.randn(1, 1, 100, 100)  # Assuming the model expects a single-channel 100x100 image
-classes = {1:'1', 2:'2', 3:'3', 4:'4', 5:'sentry', 6:'base', 7:'tower'}
+# 100x100 fake image
+fake_image = torch.randn(1, 100, 100)  # 1 channel
+
+# convert fake_image to PIL image
+fake_image = transforms.ToPILImage()(fake_image)
 
 transform = transforms.Compose([
       transforms.Grayscale(num_output_channels=1),
       transforms.ToTensor(),
       transforms.Lambda(lambda t: t.sqrt()),
-    ])
+])
 
 fake_image = transform(fake_image)
-print(classes[model(fake_image).argmax(dim=1, keepdim=True).item()])
+# add batch dimension
+fake_image = fake_image.unsqueeze(0)
 
-torch.tensor()
+classes = {1:'1', 2:'2', 3:'3', 4:'4', 5:'sentry', 6:'base', 7:'tower'}
+output = model(fake_image)
+predicted_class = classes[output.argmax(dim=1).item()]
+
+print(predicted_class)
