dff7b9d9ae873de9c8364cd1233d5486c7e08f4f
Daniel Zhu <daniel.zhu@colorado.edu>
2025-10-28 19:57:40 -0600

    added translation

--- cnn/sandbox_code.py
+++ cnn/sandbox_code.py
@@ -7,5 +7,7 @@ model = torch.load(os.getcwd() + '/models/full_model.pth', map_location=torch.de
 
 # 100, 100 fake image
 fake_image = torch.randn(1, 1, 100, 100)  # Assuming the model expects a single-channel 100x100 image
+classes = {1:'1', 2:'2', 3:'3', 4:'4', 5:'sentry', 6:'base', 7:'tower'}
 
-print(model(fake_image))
+print(classes[model(fake_image).argmax(dim=1, keepdim=True).item()])
+
