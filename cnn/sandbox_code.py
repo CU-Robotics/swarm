b44ddcdc472e35e28616948a45b43c685422383f
Daniel Zhu <daniel.zhu@colorado.edu>
2025-10-28 20:03:10 -0600

    sturcture

--- cnn/sandbox_code.py
+++ cnn/sandbox_code.py
@@ -1,5 +1,7 @@
 import torch
 import os
+import torchvision.transforms as transforms
+
 
 # load model
 
@@ -9,5 +11,13 @@ model = torch.load(os.getcwd() + '/models/full_model.pth', map_location=torch.de
 fake_image = torch.randn(1, 1, 100, 100)  # Assuming the model expects a single-channel 100x100 image
 classes = {1:'1', 2:'2', 3:'3', 4:'4', 5:'sentry', 6:'base', 7:'tower'}
 
+transform = transforms.Compose([
+      transforms.Grayscale(num_output_channels=1),
+      transforms.ToTensor(),
+      transforms.Lambda(lambda t: t.sqrt()),
+    ])
+
+fake_image = transform(fake_image)
 print(classes[model(fake_image).argmax(dim=1, keepdim=True).item()])
 
+torch.tensor()
