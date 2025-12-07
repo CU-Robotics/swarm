f77c6f666a162636bea75464318be0a687ce8052
Daniel Zhu <daniel.zhu@colorado.edu>
2025-12-06 17:52:13 -0700

    added file to run model on new camera

---
+++ run_yolo_model.py
@@ -0,0 +1,49 @@
+from pypylon import pylon
+import cv2 
+import onnxruntime as ort
+import ultralytics
+import os
+
+
+camera = pylon.InstantCamera(pylon.TlFactory.GetInstance().CreateFirstDevice())
+print("Device:", camera.GetDeviceInfo().GetModelName())
+
+camera.Open()
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+model_path = os.path.join(current_dir, "best.onnx")
+
+# Optional: set pixel format
+camera.PixelFormat.SetValue("BGR8")
+
+camera.StartGrabbing()
+
+converter = pylon.ImageFormatConverter()
+converter.OutputPixelFormat = pylon.PixelType_BGR8packed
+
+# load in yolov8 model
+model = ultralytics.YOLO(model_path)
+
+while camera.IsGrabbing():
+    grab = camera.RetrieveResult(5000, pylon.TimeoutHandling_ThrowException)
+
+    if grab.GrabSucceeded():
+        img = converter.Convert(grab)
+        frame = img.GetArray()
+
+        results = model(frame)
+
+        # show results overlaid on image
+        annotated_frame = results[0].plot()
+
+        cv2.imshow("Annotated Image", annotated_frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+
+        
+    else:
+        print("Grab failed")
+
+    grab.Release()
