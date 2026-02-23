ec0b488cf5d8c8c4ea3deffc76b37a1ba0256db4
Daniel Zhu <daniel.zhu@colorado.edu>
2026-02-23 12:41:09 -0700

    added label studio auto label backend to folder

---
+++ YOLO/label-studio-yolo-backend/README.md
@@ -0,0 +1,92 @@
+# NOTE I DID NOT CREATE THIS, Aliaksei Chymba DID!!!
+# I copied over to save on our own repo to fit our own needs.
+
+# Custom YOLO Backend for Label Studio
+
+This backend provides ML-assisted labeling capabilities to accelerate your annotation workflow, supporting both **object detection** and **instance segmentation** tasks.
+
+## 🏗️ Project Structure
+
+- **Dockerfile**: The Dockerfile for building the backend container.
+
+- **docker-compose.yml**: The docker-compose file for running the backend.
+
+- **_wsgi.py**: WSGI app initializer.
+
+- **start.sh**: bash script to start the whole process.
+
+- **model.py**: The Python code for the ML backend model.
+
+- **requirements.txt**: The list of Python dependencies for the backend.
+
+## 🚀 Quick Start
+
+1. **Clone the repository**:
+
+   ```bash
+   git clone https://github.com/seblful/label-studio-yolo-backend.git
+   cd label-studio-yolo-backend
+   ```
+
+2. **Create and prepare your model directory:**
+   
+    ```bash
+    mkdir models
+    cp /path/to/your/model.pt models/
+    ```
+
+3. **Edit `.env` with your settings:**
+   
+    ```yaml
+    BASIC_AUTH_USER=  # Optional
+    BASIC_AUTH_PASS=  # Optional
+    LOG_LEVEL=DEBUG
+
+    MODEL_FILENAME=model.pt
+
+    PORT=8080
+
+    LABEL_STUDIO_API_KEY= # API key from LS
+    TASK_TYPE=segmentation # segmentation or detection
+    ```
+
+
+4. **Deploy using the following command:**
+   
+    ```bash
+    docker compose up
+    ```
+
+5. **Add the model in project settings:**
+
+    From the project settings, select the **Model** page and click [**Connect Model**](https://labelstud.io/guide/ml#Connect-the-model-to-Label-Studio).
+    
+    Add the URL `http://locallhost:9090` and save the model as an ML backend.
+
+   ![Connect Model](https://github.com/seblful/label-studio-yolo-backend/raw/main/assets/images/connect_model.png)
+   ![Connected model](https://github.com/seblful/label-studio-yolo-backend/raw/main/assets/images/connected_model.png)
+
+
+6. **Label in interactive mode**
+
+    To use this functionality, activate **Auto-Annotation**.
+
+  ![Example annotation](https://github.com/seblful/label-studio-yolo-backend/raw/main/assets/images/annotation.png)
+
+  ### For users with internet restrictions:
+  
+  Configure Docker daemon with proxy:
+  ```json
+  {
+    "registry-mirrors": ["https://registry.docker-cn.com"]
+  }
+  ```
+
+## 📋 TODO
+
+- Add support for obb and keypoints.
+
+
+## 💁 Contributing
+
+Contributions to this project are welcome. To contribute, please submit an issue or pull request.
---
+++ YOLO/label-studio-yolo-backend/model.py
@@ -0,0 +1,195 @@
+import os
+
+from PIL import Image, ImageOps
+
+import numpy as np
+import ultralytics
+
+from label_studio_ml.model import LabelStudioMLBase
+from label_studio_ml.response import ModelResponse
+
+from label_studio_sdk._extensions.label_studio_tools.core.utils.io import get_local_path
+
+
+class YOLO(LabelStudioMLBase):
+    def setup(self) -> None:
+        # Task type
+        self.task_types = ["detection", "segmentation"]
+        self.set_task_type()
+
+        # Model and labels
+        self.model = self.load_model()
+        self.labels = self.model.names
+        self.from_name = "label"
+        self.to_name = "image"
+
+    def set_task_type(self) -> None:
+        self.task_type = os.getenv("TASK_TYPE")
+        assert self.task_type in self.task_types, \
+            f"Task type must be one \
+                of {self.task_types}, set TASK_TYPE in your .env file."
+
+        # Pred func
+        self.pred_funcs = {"detection": self.detect,
+                           "segmentation": self.segment}
+        self.pred_func = self.pred_funcs[self.task_type]
+
+    def load_model(self) -> ultralytics.YOLO:
+        model_dir = os.getenv("MODEL_DIR")
+        model_filename = os.getenv("MODEL_FILENAME")
+        model_filepath = os.path.join(model_dir, model_filename)
+        model = ultralytics.YOLO(model_filepath)
+        self.set("model_version", f"yolo-{model.task}")
+
+        return model
+
+    def load_image(self,
+                   task: dict) -> Image.Image:
+        # Get image path and task id
+        image_path = task.get("data").get("image")
+        task_id = task.get("id")
+
+        # Extract local image path
+        file_path = self.get_local_path(image_path,
+                                        task_id=task_id)
+
+        # Open image
+        image = Image.open(file_path)
+        image = ImageOps.exif_transpose(image)
+
+        return image
+
+    def predict(self,
+                tasks: list[dict],
+                **kwargs) -> ModelResponse:
+        predictions = self.pred_func(tasks, **kwargs)
+
+        return predictions
+
+    def detect(self, tasks: list[dict], **kwargs) -> ModelResponse:
+        # Create blank list with results
+        results = []
+
+        # Create variable to calcualte scores
+        score = 0
+        counter = 0
+
+        for task in tasks:
+            # Load image
+            image = self.load_image(task=task)
+
+            # Height and width of image
+            image_width, image_height = image.size
+
+            # Getting prediction using model
+            model_prediction = self.model.predict(image)
+
+            # Getting boxes from model prediction
+            for pred in model_prediction:
+                for i, box in enumerate(pred.boxes):
+
+                    # Points
+                    xyxy = box.xyxy[0].tolist()
+                    x = xyxy[0] / image_width * 100
+                    y = xyxy[1] / image_height * 100
+                    width = (xyxy[2] - xyxy[0]) / image_width * 100
+                    height = (xyxy[3] - xyxy[1]) / image_height * 100
+
+                    # Label
+                    labels = [self.labels[int(box.cls.item())]]
+
+                    result = {"from_name": self.from_name,
+                              "to_name": self.to_name,
+                              "id": str(i),
+                              "type": "rectanglelabels",
+                              "score": box.conf.item(),
+                              "original_width": image_width,
+                              "original_height": image_height,
+                              "image_rotation": 0,
+                              "value": {
+                                  "rotation": 0,
+                                  "x": x,
+                                  "y": y,
+                                  "width": width,
+                                  "height":  height,
+                                  "rectanglelabels": labels}}
+
+                    # Append prediction to predictions
+                    results.append(result)
+
+                    # Add score
+                    score += box.conf.item()
+                    counter += 1
+
+        predictions = [{"result": results,
+                       "score": score / counter,
+                        "model_version": self.model_version}]
+        
+        if counter == 0:
+            print("No predictions, setting score to 0")
+            score = 0
+            counter = 1
+
+        return ModelResponse(predictions=predictions)
+
+    def segment(self, tasks: list[dict], **kwargs) -> ModelResponse:
+        # Create blank list with results
+        results = []
+
+        # Create variable to calcualte scores
+        score = 0
+        counter = 0
+
+        for task in tasks:
+            # Load image
+            image = self.load_image(task=task)
+
+            # Height and width of image
+            image_width, image_height = image.size
+
+            # Getting prediction using model
+            model_prediction = self.model.predict(image)
+
+            # Getting mask segments, boxes from model prediction
+            for pred in model_prediction:
+                for i, (box, segm) in enumerate(zip(pred.boxes, pred.masks.xy)):
+
+                    # 2D array with poligon points
+                    points = segm / \
+                        np.array([image_width, image_height]) * 100
+                    points = points.tolist()
+
+                    # Label
+                    labels = [self.labels[int(box.cls.item())]]
+
+                    # Regions and predictions
+                    result = {"from_name": self.from_name,
+                              "to_name": self.to_name,
+                              "id": str(i),
+                              "type": "polygonlabels",
+                              "score": box.conf.item(),
+                              "original_width": image_width,
+                              "original_height": image_height,
+                              "image_rotation": 0,
+                              "value": {"points": points,
+                                        "polygonlabels": labels}}
+
+                    # Append prediction to predictions
+                    results.append(result)
+
+                    # Add score
+                    score += box.conf.item()
+                    counter += 1
+
+        if counter == 0:
+            score = 0
+            counter = 1
+
+        predictions = [{"result": results,
+                       "score": score / counter,
+                        "model_version": self.model_version}]
+
+        return ModelResponse(predictions=predictions)
+
+    def fit(self, event, data, **kwargs):
+        raise NotImplementedError("Training is not implemented yet")
